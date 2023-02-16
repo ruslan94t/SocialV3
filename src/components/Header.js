8b06220f2b4aa5043d857029a3754b19fae99019
ruslan94t <p3triri@yandex.ru>
2023-02-16 23:53:24 +0300

    14 css module

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import s from "./Header.module.css";
+
+
 
 const Header =()=>{
-    return  <header className="header">
+    return  <header className={s.header}>
         <img src="https://www.freeiconspng.com/thumbs/website-icon/website-icon-14.png" />
 
     </header>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import s from "./Profile.module.css";
+
+
+
+
+
+
 
 function Profile(props) {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img src="https://media.cnn.com/api/v1/images/stellar/prod/181010131059-australia-best-beaches-cossies-beach-cocos3.jpg?q=w_1600,h_900,x_0,y_0,c_fill/w_1280" alt=""/>
             </div>
@@ -14,10 +21,10 @@ function Profile(props) {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>post1</div>
-                    <div>post2</div>
-                    <div>post3</div>
+                <div className={s.posts}>
+                    <div className={s.item}>post1</div>
+                    <div className={s.item}>post2</div>
+
                 </div>
             </div>
 
