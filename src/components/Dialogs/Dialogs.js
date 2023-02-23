3bd0efdb4f6277b561fc4a1285537b0d7b49b5da
ruslan94t <p3triri@yandex.ru>
2023-02-23 12:03:00 +0300

    Dialogs

---
+++ src/components/Dialogs/Dialogs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+
+
+
+
+
+function Dialogs(props) {
+    return (
+        <div>dialogs</div>
+    );
+}
+
+export default Dialogs;
